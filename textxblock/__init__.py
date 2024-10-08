4eedbd21f5880bf1d889b57688cc270abbeb17f9
DilipJ98 <dilip@cklabs.in>
2024-10-08 15:08:52 +0530

    created: apps.py

--- textxblock/__init__.py
+++ textxblock/__init__.py
@@ -1,2 +1,3 @@
 from .textxblock import TextXBlock
 from .tasks import task_method 
+default_app_config = 'textxblock.apps.TextXBlockConfig'
---
+++ textxblock/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class TextXBlockConfig(AppConfig):
+    name = 'textxblock'
+    verbose_name = 'Text XBlock'
