0f7b5d9a0e04ff85417603a30b4f0d9bd73b7f01
DilipJ98 <dilip@cklabs.in>
2024-10-08 16:38:23 +0530

    added: migrations

--- textxblock/apps.py
+++ textxblock/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class TextXBlockConfig(AppConfig):
     name = 'textxblock'
     verbose_name = 'Text XBlock'
+    def ready(self):
+        from . import migrations
+        from django.core.management import call_command
