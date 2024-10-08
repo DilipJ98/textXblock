9330ce5ac01884b5a2a93872b90a6c660157a820
DilipJ98 <dilip@cklabs.in>
2024-10-08 06:49:17 +0530

    added: meta for model

--- textxblock/database.py
+++ textxblock/database.py
@@ -6,3 +6,7 @@ class User(models.Model):
     task_id = models.CharField(max_length= 70)
     code = models.TextField()
     code_result = models.BooleanField()
+
+
+    class Meta:
+        app_label = 'textxblock'
