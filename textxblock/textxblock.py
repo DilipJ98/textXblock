2cbde265eb654b784260ec3db2c95b58a3f17b09
DilipJ98 <dilip@cklabs.in>
2025-01-23 16:07:00 +0530

    added: headers into body for now

--- textxblock/textxblock.py
+++ textxblock/textxblock.py
@@ -384,21 +384,15 @@ class TextXBlock(XBlock):
 
 
     @XBlock.json_handler
-    def results_handler(self, request, suffix=''):
+    def results_handler(self, data, suffix=''):
         xblock_instance_data = str(self.scope_ids)
         block_location_id = xblock_instance_data.split("'")[-2]
         student_id = str(self.scope_ids.user_id)
-
-        if request.method != 'POST':
-            return Response(json.dumps({'status': 'error', 'message': 'Invalid request method'}), content_type='application/json')
-        
-        data = json.loads(request.body)
         print(data, "this is the data from the request............!!!!!....!!!!....!!!!.....!!!!......!!!!")
         #which will be used to get the usage key and student id from redis
-        submission_id = request.headers.get('x-submission-id')
+        submission_id = data.get('x-submission-id')
         redis_data = self.redis_client.hgetall(submission_id)
         if redis_data:
-            redis_data = {key.decode(): value.decode() for key, value in redis_data.items()}
             usage_key_from_redis = redis_data.get("usage_key")
             student_id_from_redis = redis_data.get("student_id")
             if usage_key_from_redis == block_location_id and student_id_from_redis == student_id:
