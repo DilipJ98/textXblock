64041fcd3e1e7c39fae14d58c113717eb30dea73
DilipJ98 <dilip@cklabs.in>
2025-03-28 12:04:14 +0530

    update: UI

--- textxblock/static/js/src/textxblock.js
+++ textxblock/static/js/src/textxblock.js
@@ -6,9 +6,9 @@ function TextXBlock(runtime, element) {
     metaTag.name = "viewport";
     metaTag.content = "width=device-width, initial-scale=1.0";
     document.getElementsByTagName("head")[0].appendChild(metaTag);
-    $(element)
-      .find(".answer-container-div")
-      .css({ "pointer-events": "none", opacity: "0" });
+    // $(element)
+    //   .find(".answer-container-div")
+    //   .css({ "pointer-events": "none", opacity: "0" });
 
     let editor;
     let isEditorUpdated = false;
@@ -172,7 +172,7 @@ function TextXBlock(runtime, element) {
 
     //this will be called on successfull ajax request of initail load call
     function getTaskDetails(result) {
-      console.log(result, " from get task details");
+      // console.log(result, " from get task details");
       //checks if there is any data is available
       if (result.user_code !== "") {
         //checks if the monaco editor updated with code
@@ -185,7 +185,6 @@ function TextXBlock(runtime, element) {
             $(element).find(".results-div").show();
             $(element).find(".results").text("We are fetching your results...");
             $(element).find(".results-marks").text("");
-            $(element).find(".msg").text("");
             //assigning the user input code to a varibale to use later in the code
             getUserAnswerFromDb = result.user_code;
             isEditorUpdated = true;
@@ -194,7 +193,7 @@ function TextXBlock(runtime, element) {
         //calling the task result function which will update the UI of code results
         showResults(result);
       } else {
-        console.log("no data receiving from get task details");
+        // console.log("no data receiving from get task details");
         //clearing interval if there is no data avaialbale on the intial load
         //this will effectively stop polling if there is no data available in db
         clearIntervalsFunction();
@@ -234,21 +233,46 @@ function TextXBlock(runtime, element) {
                 //an options object extra options for monaco
                 value: data.boilerplate,
                 language: data.language,
-                theme: "vs-dark",
+                theme: "vs-light",
+                automaticLayout: true,
                 padding: {
                   top: 15,
                   bottom: 10,
                 },
+                fontSize: 14,
                 minimap: {
                   enabled: false,
                 },
               }
             );
 
-            //this is will resizes the monaco editor on screen size changes
-            window.addEventListener("resize", () => {
-              if (editor) {
-                editor.layout();
+            monaco.languages.registerCompletionItemProvider("java", {
+              provideCompletionItems: function () {
+                const suggestions = [
+                  {
+                    label: "printHello",
+                    kind: monaco.languages.CompletionItemKind.Function,
+                    insertText: 'System.out.println("Hello, World!");',
+                    insertTextRules:
+                      monaco.languages.CompletionItemInsertTextRule
+                        .InsertAsSnippet,
+                    documentation: "Inserts a print statement",
+                  },
+                ];
+                console.log("Suggestions:", suggestions);
+                return { suggestions };
+              },
+            });
+
+            editor.onDidChangeModelContent((event) => {
+              if (event.changes.length > 0) {
+                setTimeout(() => {
+                  editor.trigger(
+                    "keyboard",
+                    "editor.action.triggerSuggest",
+                    {}
+                  );
+                }, 300);
               }
             });
 
@@ -270,22 +294,17 @@ function TextXBlock(runtime, element) {
 
     //on code check box it will show answer editor
     $(element)
-      .find(".ans-select")
+      .find(".output-select")
       .on("change", function (e) {
         if (isTImerEnd) {
           toggleAnswer();
         } else {
-          $(this).val("Hide");
+          $(this).val("Output");
         }
       });
 
     function toggleAnswer() {
       if (!isCheckBoxChecked && isTImerEnd) {
-        $(element).find(".answer-container-div").css({
-          "pointer-events": "none",
-          opacity: "1",
-          transition: "opacity 1s ease-in",
-        });
         $(element).find(".answer-container").text(dataFromInitiaRequest.answer);
         $(element).find(".answer-container").css({
           "pointer-events": "auto",
@@ -294,13 +313,8 @@ function TextXBlock(runtime, element) {
         });
         isCheckBoxChecked = true;
       } else {
+        $(element).find(".answer-container").text("");
         $(element).find(".answer-container").css({
-          "pointer-events": "none",
-          opacity: "0",
-          transition: "opacity 1s ease-out",
-        });
-
-        $(element).find(".answer-container-div").css({
           "pointer-events": "none",
           opacity: "0",
           transition: "opacity 1s ease-out",
@@ -318,10 +332,226 @@ function TextXBlock(runtime, element) {
 
     function toggleTheme() {
       if (!isThemeUpdated) {
-        monaco.editor.setTheme("vs-light");
+        monaco.editor.setTheme("vs-dark");
+        $(element)
+          .find(".textxblock-container")
+          .css("background-color", "black");
+
+        //code editor menu
+        $(element).find(".code-editor-menu").css({
+          "background-color": "#333",
+          border: "1px solid rgba(255, 255, 255, 0.5)",
+        });
+
+        //progress results
+        $(element).find(".progress-results").css({
+          "background-color": "#0a84ff2e",
+        });
+        $(element).find(".editors-div").css({
+          border: "1px solid rgba(255, 255, 255, 0.5)",
+          borderTop: "none",
+        });
+        $(element).find(".code-svg-div").css("color", "white");
+        //answer container div
+        $(element).find(".answer-container-div").css({
+          "background-color": "#333",
+          border: "1px solid rgba(255, 255, 255, 0.5)",
+        });
+        //out put div
+        $(element).find(".output-div").css({
+          "background-color": "#333",
+          borderBottom: "1px solid rgba(255, 255, 255, 0.5)",
+        });
+
+        //answer-container
+        $(element).find(".answer-container").css({
+          "background-color": "#1e1e1e",
+          border: "1px solid #333",
+          color: "#d4d4d4",
+        });
+
+        //.show-question-div
+        $(element).find(".show-question-div").css({
+          "background-color": "#1e1e1e",
+          border: "1px solid rgba(255, 255, 255, 0.5)",
+          color: "white",
+        });
+        //question mennu
+        $(element).find(".question-menu").css({
+          "background-color": "#333",
+          borderBottom: "1px solid #888",
+          // color: "#000",
+        });
+
+        //theme changer
+        $(element).find(".theme-changer").css({
+          "background-color": "#333",
+          color: "white",
+          border: "1px solid #888",
+        });
+
+        //timer div
+        $(element).find(".timer-div").css({
+          "background-color": "#333",
+          border: "1px solid #888",
+          color: "white",
+        });
+
+        //timer container
+        $(element).find(".timer-container").css({
+          // "background-color": "#f5f5f5",
+          color: "white",
+        });
+
+        $(element).find(".reset").css({ "background-color": "transparent" });
+
+        // //reset svg
+        $(".reset svg path:first").attr("fill", "white");
+
+        //language
+        $(element).find(".language").css({
+          "background-color": "#444",
+          color: "#ffffff99",
+          border: "1px solid #888",
+          borderTop: "none",
+          borderRight: "none",
+        });
+
+        //output select
+        $(element).find(".output-select").css({
+          background: "#444",
+          color: "#ffffff99",
+          border: "1px solid rgba(255, 255, 255, 0.5)",
+        });
+
+        //code svg over
+        $(".code-svg-div").hover(
+          function () {
+            $(this).css({
+              "background-color": " #444",
+              "border-radius": "3px",
+              // border: "1px solid #888",
+            });
+          },
+          function () {
+            $(this).css({
+              "background-color": "",
+              "border-radius": "",
+              border: "none",
+            });
+          }
+        );
+
+        //results
+        $(element).find(".results-marks").css({ color: "white" });
+        $(element).find(".results").css({ color: "white" });
+
         isThemeUpdated = true;
       } else {
-        monaco.editor.setTheme("vs-dark");
+        monaco.editor.setTheme("vs-light");
+        // $(element).find(".code-editor-menu").css("background-color", "#f5f5f5");
+        $(element).find(".code-editor-menu").css({
+          "background-color": "#f5f5f5",
+          border: "1px solid #ddd",
+        });
+        $(element).find(".progress-results").css({
+          "background-color": "#0a84ff2e",
+        });
+        $(element)
+          .find(".editors-div")
+          .css({ border: "1px solid #ddd", borderTop: "none" });
+        $(element).find(".code-svg-div").css("color", "#000");
+        $(element)
+          .find(".textxblock-container")
+          .css("background-color", "#f5f5f5");
+
+        //answer container div
+        $(element)
+          .find(".answer-container-div")
+          .css({ "background-color": "#f5f5f5", border: "1px solid #ddd" });
+        //out put div
+        $(element).find(".output-div").css({
+          "background-color": "#f5f5f5",
+          borderBottom: "1px solid #ddd",
+        });
+
+        //answer-container
+        $(element)
+          .find(".answer-container")
+          .css({ "background-color": "white", border: "none", color: "black" });
+
+        //.show-question-div
+        $(element).find(".show-question-div").css({
+          "background-color": "#f5f5f5",
+          border: "1px solid #ddd",
+          color: "#000",
+        });
+        //question mennu
+        $(element).find(".question-menu").css({
+          "background-color": "#f5f5f5",
+          borderBottom: "1px solid #ddd",
+          color: "#000",
+        });
+
+        //theme changer
+        $(element).find(".theme-changer").css({
+          "background-color": "white",
+          color: "black",
+          border: "1px solid #888",
+        });
+
+        //timer div
+        $(element)
+          .find(".timer-div")
+          .css({ "background-color": "white", border: "1px solid #888" });
+
+        //timer container
+        $(element).find(".timer-container").css({
+          // "background-color": "#f5f5f5",
+          color: "#000",
+        });
+
+        //reset svg
+        $(".reset svg path:first").attr("fill", "black");
+
+        //language
+        $(element).find(".language").css({
+          "background-color": "#f5f5f5",
+          color: "black",
+          border: "1px solid #ddd",
+          borderTop: "none",
+          borderRight: "none",
+        });
+
+        //output select
+        $(element).find(".output-select").css({
+          "background-color": "white",
+          color: "black",
+          border: "1px solid #888",
+        });
+
+        //code svg over
+        $(".code-svg-div").hover(
+          function () {
+            $(this).css({
+              "background-color": "#fff",
+              "border-radius": "3px",
+              // border: "1px solid #888",
+            });
+          },
+          function () {
+            $(this).css({
+              "background-color": "",
+              "border-radius": "",
+              border: "none",
+            });
+          }
+        );
+
+        //results
+        $(element).find(".results-marks").css({ color: "black" });
+        $(element).find(".results").css({ color: "black" });
+
         isThemeUpdated = false;
       }
     }
@@ -344,7 +574,7 @@ function TextXBlock(runtime, element) {
       $(element)
         .find("#submit")
         .css({ "pointer-events": "none", opacity: "0.5" });
-      //calling user input answer function which will get the value during submit
+      // calling user input answer function which will get the value during submit
       userInputAnswer(editor.getValue());
       clearIntervalsFunction();
     }
@@ -366,7 +596,6 @@ function TextXBlock(runtime, element) {
             .text("Error occurred, please try again.");
           $(element).find(".results").hide();
           $(element).find(".results-marks").hide();
-          $(element).find(".msg").hide();
         },
       });
     }
@@ -406,7 +635,6 @@ function TextXBlock(runtime, element) {
                 .text("Error occurred, please try again.");
               $(element).find(".results").hide();
               $(element).find(".results-marks").hide();
-              $(element).find(".msg").hide();
             },
           });
         }
@@ -416,7 +644,7 @@ function TextXBlock(runtime, element) {
     }
 
     function getTaskResult(result) {
-      console.log(result, " from handler task method response");
+      // console.log(result, " from handler task method response");
       //which manages progress bar
       $(element)
         .find("#progressBar")
@@ -435,18 +663,18 @@ function TextXBlock(runtime, element) {
             url: handlerUrl,
             data: JSON.stringify({}),
             success: (response) => {
-              console.log(
-                response,
-                " this is response from get task result handler"
-              );
-              //based on the celery task status it will updted the resul for progress bar
+              // console.log(
+              //   response,
+              //   " this is response from get task result handler"
+              // );
+              // //based on the celery task status it will updted the resul for progress bar
               if (
                 response.status !== "pending" &&
                 response.status !== "error"
               ) {
                 animateProgress(100);
               } else {
-                console.log("inside else");
+                // console.log("inside else");
                 //which ensures the progress bar not to exceed 100%
                 let finalProgressLoad = Math.min(progressLoad + 10, 100);
                 animateProgress(finalProgressLoad);
@@ -464,7 +692,6 @@ function TextXBlock(runtime, element) {
                 .text("Error occurred, please try again.");
               $(element).find(".results").hide();
               $(element).find(".results-marks").hide();
-              $(element).find(".msg").hide();
             },
           });
         }
@@ -492,7 +719,7 @@ function TextXBlock(runtime, element) {
 
     //which manages to show results and progress bar
     function showResults(result) {
-      console.log(result, " inside show results");
+      // console.log(result, " inside show results");
       if (result.status === "ready") {
         $(element).find(".progressBar-div").hide();
         $(element).find(".results-div").css({ opacity: "1" });
@@ -501,7 +728,7 @@ function TextXBlock(runtime, element) {
           .find(".results")
           .text(`Solution Correct: ${result.is_correct}`);
         $(element).find(".results-marks").text(`Marks: ${result.score}`);
-        $(element).find(".msg").text(`Message: ${result.message}`);
+        $(element).find(".answer-container").text(`${result.message}`);
 
         $(element)
           .find("#submit")
